718120c55cb80b2efba4833a5b8c1a1d0d8abe7c
Mutanoball <allkaline@yandex.ru>
2016-05-23 09:10:34 +0000

    дописал функцию пагинатора транзакций пользователяЭ

--- TransactonPaginator.php
+++ TransactonPaginator.php
@@ -4,6 +4,16 @@ class TransactonPaginator {
         $this->db = $db;
         $this->name = $name;
     }
-    function print()
+    function print(){
+    	echo "транзакции пользователя $name";
+    	$result=$this->db->query("SELECT * FROM user_transaction order by id WHERE name=$name");
+    	while ($row = $result->fetch_assoc()) {
+        	$i++;
+        	$id     = $row['id'];
+        	$name	= $row['name'];
+        	$amount = $amount['amount'];
+        	echo "Номер транзакции - ".$id."  Имя пользователя - ".$name."  Сумма зачисления - ".$amount;
+    	}
+    }
 }
 ?>
